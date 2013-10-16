27b42be2eb7c9f02440122ff7abd79c26d88a184
Umesh Awasthi <umeshawasthi@gmail.com>
2013-10-16 16:26:21 +0530

    add java doc comment

--- src/main/java/org/jsr303/validation/constant/ValidatorConstants.java
+++ src/main/java/org/jsr303/validation/constant/ValidatorConstants.java
@@ -4,12 +4,23 @@
 package org.jsr303.validation.constant;
 
 /**
- * @author Umesh A
+ * <p>Class consisting various constant values being used within 
+ * bean validator plugin </p>
+ * 
+ * <p>
+ * These values can be overridden using struts.xml file by 
+ * providing custom values.
+ * </p>
+ * 
+ * @author Umesh Awasthi
+ * @version 1.0
+ * 
  *
  */
+@SuppressWarnings( "nls" )
 public final class ValidatorConstants {
 	
-	public static final String PROVIDER_CLASS = "struts.beanValidator,providerClass";
+    public static final String PROVIDER_CLASS = "struts.beanValidator,providerClass";
 	public static final String IGNORE_XMLCONFIGURAITION="struts.beanValidator.ignoreXMLConfiguration";
 	public static final String FIELD_SEPERATOR=".";
 	public static final String MODELDRIVEN_PREFIX="model";
